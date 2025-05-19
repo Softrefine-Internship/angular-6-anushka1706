2e94ea6fc6d013555bf27b0933afc534d629c032
Anushka Solanki <anushkasolanki5548@gmail.com>
2025-05-19 16:20:21 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders: string[] = ['Male', 'Female']
   country: string[] = ['India', 'USA', 'Australia']
-  myDetails!: FormGroup;
+  myDetails: FormGroup = new FormGroup({});
+
   ngOnInit(): void {
     this.myDetails = new FormGroup({
-      'firstname': new FormControl(null, [Validators.required, Validators.maxLength(50)]),
-      'lastname': new FormControl(null, [Validators.required, Validators.maxLength(50)]),
+      'firstname': new FormControl(null, [Validators.required, Validators.maxLength(10)]),
+      'lastname': new FormControl(null, [Validators.required, Validators.maxLength(10)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl(null, [Validators.required]),
       'country': new FormControl('', [Validators.required]),
